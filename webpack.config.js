7c4f2d3140432eb5fdc8072ad260196d609059f4
Dave <david.halewoodfcc@gmail.com>
2019-03-06 12:24:51 +0000

    Add expense summary to dashboard

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = env => {
   const isProduction = env === "production";
 
   return {
-    entry: "./src/app.js",
+    entry: path.join(__dirname, "src", "app.js"),
     output: {
       path: path.join(__dirname, "public", "dist"),
       filename: "bundle.js"
